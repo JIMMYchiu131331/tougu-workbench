4b95461c3760aaa72875c813231f781a79ed4480
JIMMYchiu131331 <675244014@qq.com>
2026-08-30 04:50:33 +0800

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service Worker：网络优先 + 离线回退缓存（保证升级即时生效，断网时仍可打开） */
 'use strict';
 
-const CACHE = 'tougu-v7';
+const CACHE = 'tougu-v8';
 const ASSETS = [
   './',
   './index.html',
@@ -10,6 +10,7 @@ const ASSETS = [
   './js/store.js',
   './js/quotes.js',
   './js/news.js',
+  './js/render_image.js',
   './js/ai.js',
   './js/sync.js',
   './js/app.js',
